a5ae79f6dc2ab0d7e136e67d88f03d91f27d9603
Chris Long <34128446+chrispoq@users.noreply.github.com>
2025-05-07 12:47:44 +0200

    Update jira_proxy.py

--- jira_proxy.py
+++ jira_proxy.py
@@ -35,7 +35,7 @@ def create_jira():
 
     # Send to JIRA using user's credentials
     res = requests.post(
-        f"{JIRA_URL}/rest/api/3/issue"
+        f"{JIRA_URL}/rest/api/3/issue",
         json=payload,
         auth=HTTPBasicAuth(data["jiraEmail"], data["jiraToken"]),
         headers={"Accept": "application/json", "Content-Type": "application/json"}
